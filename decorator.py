5888e3cfe5c553cbc80150b0eb34e25071f0f776
iqbal <ddd>
2020-08-18 22:48:53 -0500

    Initial commit

---
+++ decorator.py
@@ -0,0 +1,67 @@
+def inner1(func):
+    def inner2():
+        print("Before function execution");
+        func()
+        print("After function execution")
+    return inner2
+
+@inner1
+def function_to_be_used():
+    print("This is inside the function")
+
+function_to_be_used()
+
+
+# def function1():
+#     print("Subscribe now")
+#
+# func2 = function1
+# del function1
+# func2()
+
+# def funcret(num):
+#     if num==0:
+#         return print
+#     if num==1:
+#         return sum
+#
+# a = funcret(1)
+# print(a)
+
+# def executor(func):
+#     func("this")
+#
+#
+# executor(print)
+
+def dec1(func1):
+    def nowexec():
+        print("Executing now")
+        func1()
+        print("Executed")
+
+    return nowexec
+
+
+@dec1
+def who_is_harry():
+    print("Harry is a good boy")
+
+
+# who_is_harry = dec1(who_is_harry)
+
+who_is_harry()
+
+
+def inner1(func):
+    def inner2():
+        print("Assalam Alaikum wrwb")
+        func()
+        print("khuda Hafiz")
+    return inner2
+
+@inner1
+def yah_mera_func():
+    print("mai Humaira Hoon")
+
+yah_mera_func()
